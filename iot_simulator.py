c49c004730df3580f4dd1a166c21385e6d72ad38
sobo <63167653+sob0o@users.noreply.github.com>
2025-01-29 16:44:30 +0100

    update code iot_simulator : add comments to the code

--- iot_simulator.py
+++ iot_simulator.py
@@ -1,6 +1,6 @@
 import random
 import time
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt # this package is preinstalled in dockerfile with pip
 import json  # Import JSON library
 
 # MQTT Broker Configuration
@@ -8,10 +8,13 @@ BROKER = "mosquitto"  # Service name from docker-compose.yml
 PORT = 1883
 
 # Simulation Parameters
-DEVICE_COUNT = 1
+DEVICE_COUNT = 1 # the number of devices used to send the data to the broker
 PUBLISH_INTERVAL = 2  # seconds
 
 # Initialize MQTT Client
+# The 60 second keepalive ensures that 
+# if the device doesn't send any messages within 60 seconds, 
+# the client will send a ping to the broker. else the broker will assume the client is disconnected
 client = mqtt.Client()
 
 def connect_mqtt():
@@ -26,8 +29,8 @@ def connect_mqtt():
 
 def simulate_device_data(device_id):
     # Simulate sensor data
-    temperature = round(random.uniform(20.0, 30.0), 2)
-    humidity = round(random.uniform(30.0, 70.0), 2)
+    temperature = round(random.uniform(20.0, 30.0), 2) # 20-30°C
+    humidity = round(random.uniform(30.0, 70.0), 2)    # 30-70%  random data with unifrom distribution 
     return {
         "device_id": f"device_{device_id}",
         "temperature": temperature,
@@ -40,9 +43,9 @@ def publish_data():
         for device_id in range(1, DEVICE_COUNT + 1):
             data = simulate_device_data(device_id)
             # topic = f"iot_topic/device_{device_id}/data"
-            topic = f"iot_topic/data"
+            topic = f"iot_topic/data" 
             # Serialize the dictionary as a JSON string
-            json_payload = json.dumps(data)
+            json_payload = json.dumps(data) # convert the dictionary to a JSON string !! 
             client.publish(topic, json_payload)
             print(f"Published to {topic}: {json_payload}")
         time.sleep(PUBLISH_INTERVAL)
